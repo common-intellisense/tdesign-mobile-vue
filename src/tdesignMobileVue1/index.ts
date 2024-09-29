2872fabb8f67e85f0fdcb2764d9c94049f34fe77
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-09-29 14:50:13 +0800

    chore: update

---
+++ src/tdesignMobileVue1/index.ts
@@ -0,0 +1,21 @@
+import { getComponentsMap, getPropsMap } from './mapping'
+// import directives from '../directives.json'
+
+export function tdesignMobileVue1() {
+  return {
+    uiName: 'tdesign-mobile-vue',
+    lib: 'tdesign-mobile-vue',
+    map: getPropsMap(),
+  }
+}
+
+export function tdesignMobileVue1Components() {
+  return {
+    map: getComponentsMap(),
+    isSeperatorByHyphen: false,
+    prefix: 't',
+    isReact: false,
+    lib: 'tdesign-mobile-vue',
+    // directives: directives.tdesignMobileVue1,
+  }
+}
